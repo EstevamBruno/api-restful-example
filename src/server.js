cdf3c82c5caf4335a64312fd4efa854081205d7e
Bruno Kennedy <brunokennedy16@gmail.com>
2020-06-02 01:32:46 -0300

    Applying REST concepts to an API

---
+++ src/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const data = require('./data.json')
+const app = express();
+
+app.get('/clients', (req, res) => {
+  res.json(data);
+});
+
+app.get('/clients/:id', (req, res) => {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) res.status(204).json();
+
+  res.json(client);
+});
+
+app.post('/clients', (req, res) => {
+  const { name, email } = req.body;
+
+  // save client
+
+  res.json({ name, email });
+});
+
+app.put('/clients/:id', (req, res) => {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) res.status(204).json();
+
+  const { name } = req.body;
+
+  client.name = name;
+
+  res.json(client);
+});
+
+app.delete('/clients/:id', (req, res) => {
+  const { id } = req.params;
+  const clientsFiltered = data.filter(client => client.id != id);
+
+  res.json(clientsFiltered);
+});
+
+app.listen(3333, () => {
+  console.log('Server is running');
+});
